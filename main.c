03bcf399daafde86d57254a4bacd5a74a1f07ea2
Zychlix <mzflighter@gmail.com>
2022-10-10 22:53:53 +0200

    Cleaning up

--- main.c
+++ main.c
@@ -5,25 +5,23 @@ MSG message; //message queue
 
 LRESULT CALLBACK wnd_callback(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );  //Window event callback function
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);    //Main equivalent
-
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     LPSTR wnd_class_name = "wndClass";
     WNDCLASSEX wnd_class_instance;
+
+    wnd_class_instance.hInstance = hInstance;
+    wnd_class_instance.lpszClassName = wnd_class_name;
 
     wnd_class_instance.cbSize = sizeof wnd_class_instance;
     wnd_class_instance.cbClsExtra = 0;
     wnd_class_instance.cbWndExtra = 0;
     wnd_class_instance.style = 0;
     wnd_class_instance.lpfnWndProc = wnd_callback;
-    wnd_class_instance.hInstance = hInstance;
     wnd_class_instance.hIcon = LoadIcon(NULL,IDI_APPLICATION);
     wnd_class_instance.hCursor = LoadCursor(NULL,IDC_ARROW);
     wnd_class_instance.hbrBackground = (HBRUSH)(COLOR_WINDOW | 1);
     wnd_class_instance.lpszMenuName = NULL;
-    wnd_class_instance.lpszClassName = wnd_class_name;
     wnd_class_instance.hIconSm = LoadIcon(NULL,IDI_APPLICATION);
 
     if(!RegisterClassEx(&wnd_class_instance))
