1a881263532e15444a39fa0da90db037a04069c8
Zychlix <mzflighter@gmail.com>
2022-10-14 19:22:51 +0200

    Some progress

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ MSG message; //message queue
 
 LRESULT CALLBACK wnd_callback(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );  //Window event callback function
 
-void CALLBACK Midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
+void CALLBACK midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
 
 
 typedef struct indicator_state_struct
@@ -22,7 +22,7 @@ typedef struct indicator_state_struct
     LPSTR wnd_class_name[64];
 
     UINT device_count;
-    HWND h_device_count;
+    HWND h_device_counter;
 } application_instance_t;
 
 typedef struct midi_device
@@ -34,9 +34,17 @@ typedef struct midi_device
 
 } midi_device_t;
 
+typedef struct midi_data
+{
+    uint8_t status;
+    uint8_t note;
+    uint8_t velocity;
+} midi_data_t;
+
 
 application_instance_t application;
 midi_device_t keyboard;
+
 void indicator_update(application_instance_t * instance);
 
 INT register_class_instance(HINSTANCE hinstance, INT cmdshow);
@@ -45,36 +53,36 @@ void create_window_layout(application_instance_t *instance);
 
 void midi_connect(midi_device_t * device);
 
+midi_data_t midi_data_pack(DWORD instance, DWORD param1, DWORD param2);
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 
     register_class_instance(hInstance,nCmdShow);
 
 
-    HWND main_window_handle = CreateWindowEx(WS_EX_CLIENTEDGE, application.wnd_class_name, "Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, hInstance, NULL);
+    application.h_main_window = CreateWindowEx(WS_EX_CLIENTEDGE, application.wnd_class_name, "Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, hInstance, NULL);
 
-    application.h_main_window = main_window_handle;
-
-    create_window_layout( &application);
-
-    
-    if(!main_window_handle)
+    if(!application.h_main_window)
     {
         return -1;
     }
 
-    keyboard.midi_callback = (void*)Midi_callback;\
+    create_window_layout( &application);
+
+    keyboard.midi_callback = (void*)midi_callback;
     keyboard.dev_id = 0;
     midi_connect(&keyboard);
 //    if(res== MMSYSERR_NOERROR)
 //    {
 //        MessageBox(NULL,"Dziala cos","  ",0);
 //    }
-    ShowWindow(main_window_handle, nCmdShow);
-    UpdateWindow(main_window_handle);
+    ShowWindow(application.h_main_window, nCmdShow);
+    UpdateWindow(application.h_main_window);
 
-    application.device_count = midiInGetNumDevs();
-    midiInStart(dev_handle);
+  //  application.device_count = midiInGetNumDevs();
+
     while(GetMessage(&message,NULL,0,0))
     {
         TranslateMessage(&message);
@@ -144,24 +152,24 @@ LRESULT CALLBACK wnd_callback(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 void create_window_layout(application_instance_t *instance)
 {
     CreateWindowEx(0, "STATIC", "Simple MIDI synth", WS_CHILD| WS_VISIBLE, 0 , 0 , 200, 30, instance->h_main_window, NULL, instance->hi_main_window, NULL);
-    instance->h_device_count=CreateWindowEx(0, "STATIC", "Test", WS_CHILD | WS_VISIBLE, 0 , 100 , 200, 30, instance->h_main_window, NULL, instance->hi_main_window, NULL);
- CreateWindowEx(0, "BUTTON", "Test", WS_CHILD | WS_VISIBLE, 100, 100, 150, 30, instance->h_main_window, (HMENU) TESTBUTTON_ID, instance->hi_main_window, NULL );
+    instance->h_device_counter=CreateWindowEx(0, "STATIC", "Test", WS_CHILD | WS_VISIBLE, 0 , 100 , 200, 30, instance->h_main_window, NULL, instance->hi_main_window, NULL);
+     CreateWindowEx(0, "BUTTON", "Test", WS_CHILD | WS_VISIBLE, 100, 100, 150, 30, instance->h_main_window, (HMENU) TESTBUTTON_ID, instance->hi_main_window, NULL );
 }
 
 
 void indicator_update(application_instance_t *instance)
 {
     instance->device_count = midiInGetNumDevs();
-    DestroyWindow(instance->h_device_count);
+    DestroyWindow(instance->h_device_counter);
     char buffer[64];
     sprintf(buffer, "liczba %d", instance->device_count);
-    instance->h_device_count=CreateWindowEx(0, "STATIC", buffer, WS_CHILD | WS_VISIBLE, 0 , 100 , 200, 30, instance->h_main_window, NULL, instance->hi_main_window, NULL);
+    instance->h_device_counter=CreateWindowEx(0, "STATIC", buffer, WS_CHILD | WS_VISIBLE, 0 , 100 , 200, 30, instance->h_main_window, NULL, instance->hi_main_window, NULL);
 
     ShowWindow(instance->h_main_window, instance->i_cmdshow);
     UpdateWindow(instance->h_main_window);
 }
 
-void CALLBACK Midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+void CALLBACK midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
 {
     switch(wMsg) {
         case MIM_OPEN:
@@ -172,6 +180,8 @@ void CALLBACK Midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD 
             break;
         case MIM_DATA:
             printf("wMsg=MIM_DATA, dwInstance=%08x, dwParam1=%08x, dwParam2=%08x\n", dwInstance, dwParam1, dwParam2);
+            midi_data_t data = midi_data_pack(dwInstance,dwParam1,dwParam2);
+            printf("status: %d note %d velocity %d \n", data.status, data.note, data.velocity);
             break;
         case MIM_LONGDATA:
             printf("wMsg=MIM_LONGDATA\n");
@@ -192,9 +202,25 @@ void CALLBACK Midi_callback(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD 
     return;
 }
 
-
 void midi_connect(midi_device_t * device)
 {
-    midiInOpen(&(device->dev_handle),0,device->midi_callback,NULL,CALLBACK_FUNCTION);
-    midiInStart(device->dev_handle);
+    INT device_count = midiInGetNumDevs();
+
+    if(device_count) {
+        midiInOpen(&(device->dev_handle), 0, device->midi_callback, NULL, CALLBACK_FUNCTION);
+        midiInStart(device->dev_handle);
+    }
+    else
+    {
+
+    }
+}
+
+midi_data_t midi_data_pack(DWORD instance, DWORD param1, DWORD param2)
+{
+    midi_data_t data;
+    data.status = param1;                   //As x86 is little endian, the first byte is the LSB - midi status byte
+    data.note = *(((uint8_t*)&param1)+1);   //The second byte of the variable. midi note pressed
+    data.velocity = *(((uint8_t*)&param1)+2); // midi velocity
+    return data;
 }
